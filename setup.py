d87479abd7680c15112d56ee38383e20bbf39491
ehtashamul haque <your-email@example.com>
2026-05-16 00:46:50 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from typing import List
 
 HYPEN_E_DOT='e .'
 
-def get_requirements(file_path:str)->List(str):
+def get_requirements(file_path:str)->List[str]:
     '''this function is return a list of requirements'''
 
     requirements=[]
-    with open(file_path)as file_obj:
+    with open('requirement.txt')as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
