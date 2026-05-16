a1e742322c49ca7f7e7168fadca9991c0f90ab95
ehtashamul haque <your-email@example.com>
2026-05-16 11:19:35 +0530

    Datat Transformation done

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import dill
+
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+
+    try:
+
+        # Get directory path
+        dir_path = os.path.dirname(file_path)
+
+        # Create directory
+        os.makedirs(dir_path, exist_ok=True)
+
+        # Save object
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
